906e287714c5168569809f3fed0189c480db8a8c
zexihan <zexihan@outlook.com>
2020-03-25 19:32:11 -0700

    add raycasting

--- src/components/App.js
+++ src/components/App.js
@@ -13,21 +13,31 @@ import '../static/App.css';
 var camera, controls, scene, stats, renderer, loader, pointcloud;
 var bboxHelperList = [];
 
-// var raycaster = new THREE.Raycaster();
-// raycaster.params.Points.threshold = 0.04;
-// var mouse = new THREE.Vector2();
+var raycaster = new THREE.Raycaster();
+raycaster.params.Points.threshold = 0.01;
+var mouse = new THREE.Vector2();
+
+var pointclouds;
+
+var clock = new THREE.Clock();
+var toggle = 0;
+
+var sphereGeometry = new THREE.SphereBufferGeometry(0.04, 32, 32);
+var sphereMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+sphere.scale.set(1, 1, 1);
 
 function range(start, end) {
   return (new Array(end - start + 1)).fill(undefined).map((_, i) => (i + start).toString());
 }
 
-const files = range(9998, 9999); // 0 5 10 20
-const bboxes = files;
-var fileSelected = '2000';
-const fileFolder = './data/pcd/fusion/';
-var set_tag = 'user4_1';
-// const fileFolder = './data/pcd/PC_RGB_SOR/' + set_tag;
-const bboxFolder = './data/bbox/CORNER_LABELS/' + set_tag;
+const files = range(2000, 2006);
+// const bboxes = files;
+var fileSelected = '2006';
+
+var set_nm = 'fusion';
+const fileFolder = './data/pcd/' + set_nm;
+// const bboxFolder = './data/bbox/CORNER_LABELS/' + set_nm;
 
 var markedFrames = [];
 
@@ -38,7 +48,8 @@ class App extends Component {
     this.state = {
       loaded: 0,
       intrinsic: 0,
-      extrinsic: 0
+      extrinsic: 0,
+      point: []
     };
   }
 
@@ -71,7 +82,7 @@ class App extends Component {
       1000
     );
     // camera = new THREE.OrthographicCamera( 5, -5, 3, 0, 1, 1000 );
-    camera.position.set(3,3,3);
+    camera.position.set(3, 3, 3);
     camera.up.set( 0, 0, 1 );
     
 
@@ -104,7 +115,7 @@ class App extends Component {
     this.addPointcloud();
 
     // bbox
-    this.addBbox();
+    // this.addBbox();
     
     // axis
     var axesHelper = new THREE.AxesHelper( 5 );
@@ -114,12 +125,14 @@ class App extends Component {
     stats = new Stats();
     this.mount.appendChild( stats.dom );
 
+    // sphere
+    scene.add(sphere);
 
     window.addEventListener( 'resize', this.onWindowResize, false );
 
     window.addEventListener( 'keypress', this.onKeyPress );
 
-    // window.addEventListener( 'mousemove', this.onMouseMove, false );
+    window.addEventListener( 'mousemove', this.onMouseMove, false );
   }
 
   cameraMatrix2npString = ( cameraMatrix ) => {
@@ -158,6 +171,8 @@ class App extends Component {
       points.material.size = 0.02;
       
       scene.add( pointcloud );
+
+      pointclouds = [pointcloud];
       
       // var center = points.geometry.boundingSphere.center;
       // controls.target.set( center.x, center.y, center.z );
@@ -168,38 +183,38 @@ class App extends Component {
         loaded: Math.round(xhr.loaded / xhr.total * 100)
       });
 
-      console.log( ( this.state.loaded ) + '% loaded' );
+      // console.log( ( this.state.loaded ) + '% loaded' );
   
     } );
   }
 
-  addBbox = () => {
-    if (bboxes.includes(fileSelected)) {
-      fetch(bboxFolder + '/' + fileSelected + '.txt')
-        .then((res) => res.text())
-        .then(text => {
-          var lines = text.split(/\r\n|\n/);
-          lines = lines.map(line => { return line.split(' ') });
+  // addBbox = () => {
+  //   if (bboxes.includes(fileSelected)) {
+  //     fetch(bboxFolder + '/' + fileSelected + '.txt')
+  //       .then((res) => res.text())
+  //       .then(text => {
+  //         var lines = text.split(/\r\n|\n/);
+  //         lines = lines.map(line => { return line.split(' ') });
 
-          var labels = {};
-          console.log(lines.length);
-          for (var i = 0; i < lines.length; i++) {
-            labels[i] = lines[i].slice(0, 6).map(Number);
-          }
-          // console.log(labels);
+  //         var labels = {};
+  //         console.log(lines.length);
+  //         for (var i = 0; i < lines.length; i++) {
+  //           labels[i] = lines[i].slice(0, 6).map(Number);
+  //         }
+  //         // console.log(labels);
 
-          for (var i = 0; i < lines.length; i++) {
-            var bbox = new THREE.Box3();
-            // bbox.setFromCenterAndSize( new THREE.Vector3( labels[i][3], labels[i][4], labels[i][5] ), new THREE.Vector3( labels[i][1], labels[i][2], labels[i][0] ) );
-            bbox.set(new THREE.Vector3( labels[i][0], labels[i][1], labels[i][2] ), new THREE.Vector3( labels[i][3], labels[i][4], labels[i][5] ))
-            var bboxHelper = new THREE.Box3Helper( bbox, 0x00FF00 );
-            bboxHelperList.push(bboxHelper);
-            scene.add( bboxHelper );
-          }
+  //         for (var i = 0; i < lines.length; i++) {
+  //           var bbox = new THREE.Box3();
+  //           // bbox.setFromCenterAndSize( new THREE.Vector3( labels[i][3], labels[i][4], labels[i][5] ), new THREE.Vector3( labels[i][1], labels[i][2], labels[i][0] ) );
+  //           bbox.set(new THREE.Vector3( labels[i][0], labels[i][1], labels[i][2] ), new THREE.Vector3( labels[i][3], labels[i][4], labels[i][5] ))
+  //           var bboxHelper = new THREE.Box3Helper( bbox, 0x00FF00 );
+  //           bboxHelperList.push(bboxHelper);
+  //           scene.add( bboxHelper );
+  //         }
           
-        })
-    }
-  }
+  //       })
+  //   }
+  // }
 
   removePointcloud = () => {
     scene.remove( pointcloud );
@@ -232,31 +247,39 @@ class App extends Component {
 
   renderScene = () => {
 
+    camera.updateMatrixWorld();
+
     // update the picking ray with the camera and mouse position
-    // raycaster.setFromCamera( mouse, camera );
+    raycaster.setFromCamera(mouse, camera);
 
     // calculate objects intersecting the picking ray
-    // var intersects = raycaster.intersectObjects( scene.children );
-    
-    // for ( var i = 0; i < intersects.length; i++ ) {
+    if (typeof pointclouds !== "undefined") {
+      var intersections = raycaster.intersectObjects(pointclouds);
+      var intersection = (intersections.length) > 0 ? intersections[0] : null;
+      if (toggle > 0.02 && intersection != null) {
+        this.setState({
+          point: intersection.point
+        });
+        sphere.position.copy(intersection.point);
 
-    //   intersects[ i ].object.material.color.set( 0xff0000 );
+        toggle = 0;
+      }
+    }
 
-    // }
+    toggle += clock.getDelta();
 
     renderer.render(scene, camera);
 
   }
 
-  // onMouseMove = ( event ) => {
-
-  //   // calculate mouse position in normalized device coordinates
-  //   // (-1 to +1) for both components
+  onMouseMove = ( event ) => {
+    // calculate mouse position in normalized device coordinates
+    // (-1 to +1) for both components
+    event.preventDefault();
+    mouse.x = ( event.clientX / (0.8 * window.innerWidth) ) * 2 - 1;
+    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
   
-  //   mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-  //   mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-  
-  // }
+  }
 
   onWindowResize = () => {
     
@@ -269,6 +292,7 @@ class App extends Component {
   onKeyPress = ( e ) => {
     console.log(e.keyCode);
     var points = scene.getObjectByName( fileSelected + '.pcd' );
+    var flag;
     switch ( e.keyCode ) {
       case 61:
         points.material.size *= 1.2;
@@ -287,7 +311,7 @@ class App extends Component {
           fileSelected = files[files.indexOf(fileSelected) + 1]
           this.onFileNext();
 
-          var flag = false;
+          flag = false;
           for (const markedFrame of markedFrames) {
               if (markedFrame[0] === fileSelected)
                   flag = true;
@@ -304,7 +328,7 @@ class App extends Component {
           fileSelected = files[files.indexOf(fileSelected) - 1]
           this.onFilePrev();
 
-          var flag = false;
+          flag = false;
           for (const markedFrame of markedFrames) {
               if (markedFrame[0] === fileSelected)
                   flag = true;
@@ -347,7 +371,7 @@ class App extends Component {
     this.removeBbox();
 
     this.addPointcloud();
-    this.addBbox();
+    // this.addBbox();
   }
 
   onFileNext = () => {
@@ -355,7 +379,7 @@ class App extends Component {
     this.removeBbox();
 
     this.addPointcloud();
-    this.addBbox();
+    // this.addBbox();
   }
 
   onFilePrev = () => {
@@ -363,16 +387,16 @@ class App extends Component {
     this.removeBbox();
 
     this.addPointcloud();
-    this.addBbox();
+    // this.addBbox();
   }
   
   render() {
     return (
       <div>
         
-        <div id="info-mouse" className="d-none d-sm-block">
+        {/* <div id="info-mouse" className="d-none d-sm-block">
 
-          <div>Point Cloud Viewer by <a href="https://zexihan.com" target="_blank" rel="noopener">Zexi Han</a></div>
+          <div>Point Cloud Viewer by <a href="https://zexihan.com" target="_blank" rel="noopener noreferrer">Zexi Han</a></div>
           <div>axis: <font style={{color:'red'}}>X</font>  <font style={{color:'lime'}}>Y</font> <font style={{color:'blue'}}>Z</font></div>
           <div>left mouse button + move: Pan the map</div>
           <div>right mouse button + move: Rotate the view</div>
@@ -383,9 +407,9 @@ class App extends Component {
           <div>f: Mark</div>
           {this.state.loaded !== 100 && <div>{this.state.loaded}% loaded</div>}
 
-        </div>
-        <div id="info-touch" className="d-sm-none">
-          <div>Point Cloud Viewer by <a href="https://zexihan.com" target="_blank" rel="noopener">Zexi Han</a></div>
+        </div> */}
+        {/* <div id="info-touch" className="d-sm-none">
+          <div>Point Cloud Viewer by <a href="https://zexihan.com" target="_blank" rel="noopener noreferrer">Zexi Han</a></div>
           <div>one finger: Pan the map</div>
           <div>two fingers: Scale and rotate the view</div>
           <div>{this.state.loaded}% loaded</div>
@@ -396,15 +420,29 @@ class App extends Component {
           </button>
           <div className="dropdown-menu d-sm-none" aria-labelledby="framesBtn">
             {files.map((filename, i) =>
-              <a key={i} className="dropdown-item" id={filename} href="#" onClick={this.onFileSelect}>{filename}</a>
+              <button key={i} className="dropdown-item" id={filename} onClick={this.onFileSelect}>{filename}</button>
             )}
           </div>
-        </div>
+        </div> */}
         <div id="filelist" className="row d-none d-sm-block">
           <div className="col">
-            
-            <div class="alert alert-info">
-              <strong>{set_tag} {fileSelected}</strong> 
+            <h3>Point Cloud Viewer</h3>
+            <div>Suning Commerce R&D Center USA</div>
+            <div>Applied AI Lab</div>
+            <div><a href="https://zexihan.com" target="_blank" rel="noopener noreferrer">Zexi Han</a></div>
+            <br />
+            <div>axis: <font style={{ color: 'red' }}>X</font>  <font style={{ color: 'lime' }}>Y</font> <font style={{ color: 'blue' }}>Z</font></div>
+            <div>left mouse button + move: Pan the map</div>
+            <div>right mouse button + move: Rotate the view</div>
+            <div>mouse wheel: Zoom up and down</div>
+            <div>a/d: Previous/Next frame</div>
+            <div>+/-: Increase/Decrease point size</div>
+            <div>c: Change color</div>
+            <div>f: Mark</div>
+            {this.state.loaded !== 100 && <div>{this.state.loaded}% loaded</div>}
+            <br />
+            <div className="alert alert-info">
+              <strong>{set_nm} {fileSelected}</strong> 
             </div>
             
             <div className="list-group" id="list-tab" role="tablist">
@@ -420,28 +458,33 @@ class App extends Component {
               )}
             </div>
 
-            {/* <div class="my-2 p-2 d-none d-sm-block" id="matrices">
+            {/* <div className="my-2 p-2 d-none d-sm-block" id="matrices">
               <div>{"Mint = " + this.state.intrinsic}</div>
               <br />
               <div>{"Mext = " + this.state.extrinsic}</div>
             </div> */}
             
-            <div class="alert alert-success" role="alert">
+            <div className="alert alert-success" role="alert">
               Marked!
             </div>
             <CSVLink 
               data={markedFrames} 
               enclosingCharacter={``}
-              filename={"marked_frames_"+ set_tag + ".txt"}
+              filename={"marked_frames_"+ set_nm + ".txt"}
               className="btn btn-light">
                 Download marks
             </CSVLink>
+            <div>
+              <p>x: {this.state.point.x ? this.state.point.x.toFixed(4) : 0}&nbsp;
+                 y: {this.state.point.y ? this.state.point.y.toFixed(4) : 0}&nbsp;
+                 z: {this.state.point.z ? this.state.point.z.toFixed(4) : 0}</p>
+            </div>
 
           </div>
         </div>
         {/* <button className="btn btn-dark">BUTTON</button> */}
         <div
-          style={{ width: window.innerWidth, height: window.innerHeight }}
+          style={{ width: 0.8 * window.innerWidth, height: window.innerHeight }}
           ref={(mount) => { this.mount = mount }}
         />
         
